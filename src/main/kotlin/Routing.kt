6331f1d7e7f6c1ae76cf7e1b38ff982e62d58b17
m.makhovyk <m.makhovyk@letyshops.com>
2025-08-01 16:44:09 +0300

    add tasks get requests

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -5,6 +5,9 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import model.Priority
+import model.TaskRepository
+import model.tasksAsTable
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -26,6 +29,37 @@ fun Application.configureRouting() {
 
         get("/error-test") {
             throw IllegalStateException("Too Busy")
+        }
+
+        get("/tasks") {
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = TaskRepository.allTasks().tasksAsTable()
+            )
+        }
+
+        get("/tasks/byPriority/{priority?}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
